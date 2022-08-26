2c52863b27d8f06fc5c8e7909ac53645c7ecb949
TheCrunchy <crunchycamsta@gmail.com>
2022-08-26 21:51:49 +0100

    .

---
+++ PlayerProductionUpgrades/Helpers/FileUtils.cs
@@ -0,0 +1,69 @@
+﻿using System.IO;
+using System.Xml.Serialization;
+using Newtonsoft.Json;
+
+namespace PlayerProductionUpgrades.Helpers
+{
+    public class FileUtils
+    {
+        public void WriteToJsonFile<T>(string FilePath, T ObjectToWrite, bool Append = false) where T : new()
+        {
+            TextWriter writer = null;
+            try
+            {
+                var contentsToWriteToFile = JsonConvert.SerializeObject(ObjectToWrite, Newtonsoft.Json.Formatting.Indented, new Newtonsoft.Json.Converters.StringEnumConverter());
+                writer = new StreamWriter(FilePath, Append);
+                writer.Write(contentsToWriteToFile);
+            }
+            finally
+            {
+                writer?.Close();
+            }
+        }
+
+        public T ReadFromJsonFile<T>(string FilePath) where T : new()
+        {
+            TextReader reader = null;
+            try
+            {
+                reader = new StreamReader(FilePath);
+                var fileContents = reader.ReadToEnd();
+                return JsonConvert.DeserializeObject<T>(fileContents);
+            }
+            finally
+            {
+                reader?.Close();
+            }
+        }
+        public void WriteToXmlFile<T>(string FilePath, T ObjectToWrite, bool Append = false) where T : new()
+        {
+            TextWriter writer = null;
+            try
+            {
+                var serializer = new XmlSerializer(typeof(T));
+                writer = new StreamWriter(FilePath, Append);
+                serializer.Serialize(writer, ObjectToWrite);
+            }
+            finally
+            {
+                writer?.Close();
+            }
+        }
+
+        public T ReadFromXmlFile<T>(string FilePath) where T : new()
+        {
+            TextReader reader = null;
+            try
+            {
+                var serializer = new XmlSerializer(typeof(T));
+                reader = new StreamReader(FilePath);
+                return (T)serializer.Deserialize(reader);
+            }
+            finally
+            {
+                reader?.Close();
+            }
+        }
+    }
+}
+
